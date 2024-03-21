c9774e8dcd35b2f8e1876e211f92e01b285e2df1
fatima-elfat <ffzelfatehy@gmail.com>
2024-03-21 16:58:11 +0000

    fix errors

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Task 3: Old school band.
-SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
+SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
-WHERE style LIKE 'Glam rock%'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
