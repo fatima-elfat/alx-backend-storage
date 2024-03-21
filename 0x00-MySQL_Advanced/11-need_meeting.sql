d87259d6649bcfaa8149628ef19b0071e36734a8
fatima-elfat <ffzelfatehy@gmail.com>
2024-03-21 17:58:43 +0000

    add files of task 10 and 11

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,6 @@
+-- Task 11: No table for a meeting.
+CREATE VIEW need_meeting AS 
+SELECT name FROM students 
+WHERE 
+    score < 80 AND (last_meeting IS NULL OR 
+        last_meeting < ADDDATE(CURDATE(), interval -1 MONTH));
