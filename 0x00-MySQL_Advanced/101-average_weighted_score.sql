0717ace35f93ae55ad9358e6b48c671049301f88
fatima-elfat <ffzelfatehy@gmail.com>
2024-03-21 19:25:03 +0000

    fix errors

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -2,20 +2,20 @@
 DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
 BEGIN
-    DECLARE avg_score FLOAT;
-    DECLARE u_id INT;
-    SELECT SUM(score * weight) / SUM(weight)
+    DECLARE avg_score FLOAT DEFAULT 1;
+    DECLARE u_id INT DEFAULT 0;
+    SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
         INTO avg_score
         FROM users
         JOIN corrections ON users.id=corrections.user_id 
         JOIN projects ON corrections.project_id=projects.id 
-        GROUP BY users.id
+        GROUP BY users.id;
     SELECT users.id
         INTO u_id
         FROM users
         JOIN corrections ON users.id=corrections.user_id 
         JOIN projects ON corrections.project_id=projects.id 
-        GROUP BY users.id
+        GROUP BY users.id;
     UPDATE users SET users.average_score = avg_score WHERE users.id=u_id;
 END //
 DELIMITER ;
