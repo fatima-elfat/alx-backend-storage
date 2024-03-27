41b93c45ef9b73624a7191c61ec967614a5caacf
fatima-elfat <ffzelfatehy@gmail.com>
2024-03-27 01:08:09 +0000

    try fixing

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -12,38 +12,22 @@ def get_stats(nginx_collection):
     """
     _logs = nginx_collection.count_documents({})
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    """print("{} logs".format(_logs))"""
-    print(f"{_logs} logs")
-    print("Methods:")
+    """print('{} logs'.format(_logs))"""
+    print(f'{_logs} logs')
+    print('Methods:')
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        print(f'\tmethod {method}: {count}')
     count = nginx_collection.count_documents(
         {
             "method": "GET",
             "path": "/status"
         })
-    """print("{} status check".format(count))"""
-    print(f"{count} status check")
+    """print('{} status check'.format(count))"""
+    print(f'{count} status check')
 
 
 if __name__ == "__main__":
-    server = "mongodb://127.0.0.1:27017"
+    server = 'mongodb://127.0.0.1:27017'
     client = MongoClient(server)
-    nginx_collection = client.logs.nginx
-    """get_stats(client.logs.nginx)"""
-    _logs = nginx_collection.count_documents({})
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    """print("{} logs".format(_logs))"""
-    print(f"{_logs} logs")
-    print("Methods:")
-    for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count} ")
-    count = nginx_collection.count_documents(
-        {
-            "method": "GET",
-            "path": "/status"
-        })
-    """print("{} status check".format(count))"""
-    print(f"{count} status check")
+    get_stats(client.logs.nginx)
