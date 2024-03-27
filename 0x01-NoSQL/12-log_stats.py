af86f87eaecd02cd408feb7071baaf73cc8cbef0
fatima-elfat <ffzelfatehy@gmail.com>
2024-03-27 01:05:14 +0000

    try fixing

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -30,4 +30,20 @@ def get_stats(nginx_collection):
 if __name__ == "__main__":
     server = 'mongodb://127.0.0.1:27017'
     client = MongoClient(server)
-    get_stats(client.logs.nginx)
+    nginx_collection = client.logs.nginx
+    """get_stats(client.logs.nginx)"""
+    _logs = nginx_collection.count_documents({})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    """print('{} logs'.format(_logs))"""
+    print(f'{_logs} logs')
+    print('Methods:')
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
+    count = nginx_collection.count_documents(
+        {
+            "method": "GET",
+            "path": "/status"
+        })
+    """print('{} status check'.format(count))"""
+    print(f'{count} status check')
