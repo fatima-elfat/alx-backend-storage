4b5e8247ebab292633a8f8b176eb2e864dc919ed
fatima-elfat <ffzelfatehy@gmail.com>
2024-03-27 00:53:21 +0000

    print using string literal

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -12,17 +12,19 @@ def get_stats(nginx_collection):
     """
     _logs = nginx_collection.count_documents({})
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print('{} logs'.format(_logs))
+    """print('{} logs'.format(_logs))"""
+    print(f'{_logs} logs')
     print('Methods:')
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
-        print('\tmethod {}: {}'.format(method, count))
+        print(f'\tmethod {method}: {count}')
     count = nginx_collection.count_documents(
         {
             "method": "GET",
             "path": "/status"
         })
-    print('{} status check'.format(count))
+    """print('{} status check'.format(count))"""
+    print(f'{count} status check')
 
 
 if __name__ == "__main__":
